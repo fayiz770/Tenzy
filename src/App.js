5a5b00ae8ca18008432d6cc55db3380e8c8080e1
fayiz770 <mohammadwaris456@gmail.com>
2023-07-02 14:52:30 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Die from './Components/Die';
+
+function App() {
+  return (
+    <div className="App">
+      <Die value="1" />
+      <Die value="2" />
+      <Die value="3" />
+      <Die value="4" />
+      <Die value="5" />
+      <Die value="6" />
+      <Die value="7" />
+      <Die value="8" />
+      <Die value="9" />
+      <Die value="10" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Die.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Die(props) {
+    return(
+        <div className='die'>
+            <h1>{props.value}</h1>
+        </div>
+    )
+};
